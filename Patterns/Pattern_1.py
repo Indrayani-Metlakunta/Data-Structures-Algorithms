d23251498b93b97fc15305dc09e35b2a87063432
indrayani9 <lnu.in@northeastern.edu>
2024-08-06 17:52:36 -0400

    Pattern 1

---
+++ Patterns/Pattern_1.py
@@ -0,0 +1,9 @@
+def pattern_1():
+    N = int(input("Enter the number: "))
+    for i in range(N):
+        for j in range(N):
+            print("*" , end=" ")
+        print()
+
+pattern_1()
+    
