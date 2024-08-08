96bcb6eb3f1126ffdceb52557a3173fcda6743bd
indrayani9 <lnu.in@northeastern.edu>
2024-08-08 12:56:54 -0400

    Pattern 7

---
+++ Patterns/Pattern_7.py
@@ -0,0 +1,23 @@
+# https://www.geeksforgeeks.org/problems/triangle-pattern-1661492263/1
+
+def printPyramid():
+    N = int(input("Enter :"))
+    for i in range(N):
+        for j in range(N-i-1):
+            print(" ", end="")
+        for j in range(2*i+1):
+            print('*', end="")
+        for j in range(N-i-1):
+            print(" ", end = "")
+        print()
+
+printPyramid()
+
+# o/p:
+#   *
+#  ***
+# *****
+
+                
+
+
