369bed00c5202270c51fb87c1574465909200b98
indrayani9 <lnu.in@northeastern.edu>
2024-08-08 13:09:29 -0400

    Pattern 8

---
+++ Patterns/Pattern_8.py
@@ -0,0 +1,10 @@
+def printTriangle():
+    N = int(input("enter:"))
+    for i in range(N):
+        for j in range(i):
+            print(" ",end="")
+        for j in range(2*N-2*i-1):
+            print("*",end="")
+        print()
+
+printTriangle()
